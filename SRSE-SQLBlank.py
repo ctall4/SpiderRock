bdbc4065236cbb514097d52d1aebc80ff4208fc3
ctall4 <97898695+ctall4@users.noreply.github.com>
2022-04-26 08:26:28 -0400

    Create SRSE-SQLBlank.py

---
+++ SRSE-SQLBlank.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 08:22:22 2022
+
+@author: ctall
+"""
+
+import mysql.connector
+
+cnx = mysql.connector.connect(user='ekjc.ctall', password='CzM809$',
+                              host='127.0.0.1',
+                              database='employees')
+cnx.close()
